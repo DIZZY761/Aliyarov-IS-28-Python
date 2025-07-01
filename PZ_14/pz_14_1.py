d6cc05493a0c6dce1cc4caa1c03219991adcd00e
DIZZY761 <raliyarov200710@gmail.com>
2025-07-01 19:30:17 +0300

    Create pz_14_1.py

---
+++ PZ_14/pz_14_1.py
@@ -0,0 +1,29 @@
+# Функция для чтения фамилий писателей из файла
+def read_writer_surnames(filename):
+    surnames = []
+    with open('C:\Users\Рустам\Documents\GitHub\Aliyarov-IS-28-Python\PZ_14\writer.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            surname = line.strip()  # Удаление пробелов и символов новой строки
+            if surname:  # Проверяем, что строка не пустая
+                surnames.append(surname)
+    return surnames
+
+# Функция для замены слова "роман" на "произведение"
+def replace_word(input_filename, output_filename):
+    with open(input_filename, 'r', encoding='utf-8') as infile:
+        content = infile.read()
+    
+    # Замена слова
+    modified_content = content.replace('роман', 'произведение')
+    
+    with open(output_filename, 'w', encoding='utf-8') as outfile:
+        outfile.write(modified_content)
+
+# Основная часть программы
+if __name__ == '__main__':
+    # Чтение фамилий
+    surnames = read_writer_surnames('writer.txt')
+    print(f"Количество фамилий: {len(surnames)}")
+    
+    # Замена слов в файле
+    replace_word('text_with_romans.txt', 'text_with_proizvedeniyami.txt')
